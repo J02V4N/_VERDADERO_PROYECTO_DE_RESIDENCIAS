bc0d2f7f0e812807f4a585edf2054e9b8a39c030
J02V4N <josegamer58@hotmail.com>
2025-10-13 12:24:17 -0600

    esto de aqui tiene errores asi que , esperare

--- PROYECTO_RESIDENCIAS/obj/x86/Debug/net8.0-windows/PROYECTO_RESIDENCIAS.AssemblyInfo.cs
+++ PROYECTO_RESIDENCIAS/obj/x86/Debug/net8.0-windows/PROYECTO_RESIDENCIAS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTO_RESIDENCIAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9790cbb0df839364b98282eb713c64acc5f42e5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f52df93005940fdfee5969664fe9d47128f0348")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTO_RESIDENCIAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTO_RESIDENCIAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
